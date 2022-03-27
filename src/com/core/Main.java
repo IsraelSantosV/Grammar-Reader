d998ca359363d521953a384720b3503ef0b4e7a7
Israel Santos Vieira <IsraelS_Vieira@hotmail.com>
2022-03-27 00:34:55 -0300

    Changed project language

--- src/com/core/Main.java
+++ src/com/core/Main.java
@@ -14,7 +14,10 @@ public class Main extends Application {
     public static final String TRANSLATION_FILE = "Translation.json";
     public static final String SYNTAX_DEFINITIONS_FILE = "SyntaxDefinitions.json";
 
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
